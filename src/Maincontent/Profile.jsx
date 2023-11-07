2e7649e6da2b3e57c3e6cec99c162b29b1764acd
Oliver Galante <oliverandcarli@gmail.com>
2023-11-07 16:18:05 -0500

    Fixed username now allowing username to display.

--- src/Maincontent/Profile.jsx
+++ src/Maincontent/Profile.jsx
@@ -4,9 +4,10 @@ import React, { useState, useEffect } from "react";
 const Profile = () => {
 	const [userData, setUserData] = useState(null);
 	const userId = localStorage.getItem("userId"); // Get the userId from local storage
+	const userName = localStorage.getItem("username"); // Get the username from local storage
 
 	useEffect(() => {
-		const url = "http://127.0.0.1:8000/profiles/${userId}"; // Make a GET request to your API endpoint with the userId
+		const url = `http://127.0.0.1:8000/profiles/${userId}`; // Make a GET request to your API endpoint with the userId
 		fetch(url)
 			.then((response) => response.json())
 			.then((data) => setUserData(data))
@@ -16,9 +17,8 @@ const Profile = () => {
 	return (
 		<div className="profile-section" style={{ width: "25%" }}>
 			{userData && (
-				<div>
-					<h1>Hello {userData.name}</h1>
-					<p>{userData.bio}</p>
+				<div className="profile-user-name">
+					<h1>Hello {userName}</h1>
 					{/* Add more profile information here */}
 				</div>
 			)}
