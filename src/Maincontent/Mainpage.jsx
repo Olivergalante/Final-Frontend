586160c016f2e45184ed5608b5fd11f1e6aabb47
Oliver Galante <oliverandcarli@gmail.com>
2023-11-07 11:24:03 -0500

    Fixed changes added by moving file structure around :).

--- src/Maincontent/Mainpage.jsx
+++ src/Maincontent/Mainpage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Profile from "./Profile";
 import Content from "./Content";
-import "./Content.css";
+import "./mainpage.css";
 
 export function MainPage() {
 	return (
