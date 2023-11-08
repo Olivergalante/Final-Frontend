897bad8a4d71a26249d038630d7ec64070eadb0f
Oliver Galante <oliverandcarli@gmail.com>
2023-11-08 16:33:25 -0500

    Posting feature is complete.

--- src/Maincontent/CreatePost.jsx
+++ src/Maincontent/CreatePost.jsx
@@ -1,11 +1,37 @@
 import React, { useState } from "react";
 
 const CreatePost = () => {
-	const [title, setTitle] = useState("");
-	const [content, setContent] = useState("");
+	const [title, setTitle] = useState(""); // State variable for title
+	const [content, setContent] = useState(""); // State variable for content
 
 	const handlePost = () => {
-		// Implement the POST request here using fetch.
+		const userId = localStorage.getItem("userId"); // Get the userId from local storage
+		const endpoint = "http://127.0.0.1:8000/posts/";
+
+		// Define the data you want to send in the request
+		const postData = {
+			title: title, // Use the state variable for title
+			content: content, // Use the state variable for content
+			author: userId, // Use the userId from local storage
+		};
+
+		// Make the POST request using the fetch API
+		fetch(endpoint, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(postData),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				// Handle the response from the server here
+				console.log(data);
+			})
+			.catch((error) => {
+				// Handle any errors here
+				console.error("Error:", error);
+			});
 	};
 
 	return (
@@ -22,7 +48,7 @@ const CreatePost = () => {
 				value={content}
 				onChange={(e) => setContent(e.target.value)}
 			/>
-			<button>Post</button>
+			<button onClick={handlePost}>Post</button>
 		</div>
 	);
 };
