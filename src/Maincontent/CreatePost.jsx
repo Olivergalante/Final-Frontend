e039701ab1b6f5e7148fc63e955e9ce4ffc7d16f
Oliver Galante <oliverandcarli@gmail.com>
2023-11-10 08:25:46 -0500

    Added images

--- src/Maincontent/CreatePost.jsx
+++ src/Maincontent/CreatePost.jsx
@@ -2,37 +2,38 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
-	const [title, setTitle] = useState(""); // State variable for title
-	const [content, setContent] = useState(""); // State variable for content
+	const [title, setTitle] = useState("");
+	const [content, setContent] = useState("");
+	const [image, setImage] = useState(null);
 
 	const navigate = useNavigate();
+
+	const handleImageChange = (e) => {
+		setImage(e.target.files[0]);
+		console.log(image);
+	};
+
 	const handlePost = () => {
-		const userId = localStorage.getItem("userId"); // Get the userId from local storage
+		const userId = localStorage.getItem("userId");
 		const endpoint = "http://127.0.0.1:8000/posts/";
 
-		// Define the data you want to send in the request
-		const postData = {
-			title: title, // Use the state variable for title
-			content: content, // Use the state variable for content
-			author: userId, // Use the userId from local storage
-		};
+		const formData = new FormData();
+		formData.append("title", title);
+		formData.append("content", content);
+		formData.append("author", userId);
+		formData.append("image", image);
+		formData.append("username", localStorage.getItem("username"));
 
-		// Make the POST request using the fetch API
 		fetch(endpoint, {
 			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(postData),
+			body: formData,
 		})
 			.then((response) => response.json())
 			.then((data) => {
-				// Handle the response from the server here
 				console.log(data);
-				navigate("/mainpage");
+				// navigate("/mainpage");
 			})
 			.catch((error) => {
-				// Handle any errors here
 				console.error("Error:", error);
 			});
 	};
@@ -56,6 +57,12 @@ const CreatePost = () => {
 				placeholder="Content"
 				value={content}
 				onChange={(e) => setContent(e.target.value)}
+			/>
+			<input
+				type="file"
+				name="image_url"
+				accept="image/jpeg,image/png,image/gif"
+				onChange={handleImageChange}
 			/>
 			<div className="button-container">
 				<button className="button-post" onClick={handlePost}>
